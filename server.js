751bcd47ed39ef7f0a6cd27e3474b268ebcc592a
Kathryn <kathrynleee@users.noreply.github.com>
2021-03-03 16:49:35 +0100

    port

--- server.js
+++ server.js
@@ -3,19 +3,15 @@ var cors = require('cors')
 var http = require('http')
 var fs = require('fs')
 var app = express()
-// var path = require('path')
-var port = process.env.PORT || 3000
-var htmlPort = process.env.PORT || 8080
+
+var port = 3000
+var htmlPort = process.env.PORT || 8080 
 
 var dataController = require('./data')
 
 app.options('*', cors())
 app.use(cors())
 app.use('/api/data', dataController)
-
-// var root = path.normalize(__dirname + '/..')
-// var client = path.join(root, 'client', 'dist')
-// app.use(express.static(client))
 
 app.get('/api', (req, res) => {
   res.send('Hello World!')
