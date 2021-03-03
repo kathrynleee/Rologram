9c34275ed1b293f4756808235cac559bdbed98e2
Kathryn <kathrynleee@users.noreply.github.com>
2021-03-03 17:06:24 +0100

    remove http server

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var http = require('http')
 var fs = require('fs')
 var app = express()
 
-var port = 3000
-var htmlPort = process.env.PORT || 8080 
+var port = process.env.PORT || 3000
+//var htmlPort = process.env.PORT || 8080 
 
 var dataController = require('./data')
 
@@ -14,28 +14,18 @@ app.use(cors())
 app.use('/api/data', dataController)
 
 app.get('/api', (req, res) => {
-  res.send('Hello World!')
+  res.send(`${port}`)
 })
 
 app.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`)
 })
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream(__dirname + '/html/index.html').pipe(res)
-})
-
-server.listen(htmlPort, () => {
-    console.log(`Frontend listening at http://localhost:${htmlPort}`)
-})
-
-//fs.readFile(__dirname + '/html/index.html', (err, html) => {
-//    if (err) throw err
+//const server = http.createServer((req, res) => {
+//  res.writeHead(200, { 'content-type': 'text/html' })
+//  fs.createReadStream(__dirname + '/html/index.html').pipe(res)
+//})
 //
-//    http.createServer(function(request, response) {  
-//        response.writeHeader(200, {"Content-Type": "text/html"})
-//        response.write(html) 
-//        response.end()
-//    }).listen(htmlPort)
+//server.listen(htmlPort, () => {
+//    console.log(`Frontend listening at http://localhost:${htmlPort}`)
 //})
