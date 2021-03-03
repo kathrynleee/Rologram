791e53517c2bd582ca5024b678145bcae1fd6a04
Kathryn <kathrynleee@users.noreply.github.com>
2021-03-03 16:41:43 +0100

    node port problem

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var http = require('http')
 var fs = require('fs')
 var app = express()
 // var path = require('path')
-var port = 3000
-var htmlPort = 8080
+var port = process.env.PORT || 3000
+var htmlPort = process.env.PORT || 8080
 
 var dataController = require('./data')
 
@@ -25,13 +25,21 @@ app.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`)
 })
 
-
-fs.readFile(__dirname + '/html/index.html', (err, html) => {
-    if (err) throw err
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"})
-        response.write(html) 
-        response.end()
-    }).listen(htmlPort)
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream(__dirname + '/html/index.html').pipe(res)
 })
+
+server.listen(htmlPort, () => {
+    console.log(`Frontend listening at http://localhost:${htmlPort}`)
+})
+
+//fs.readFile(__dirname + '/html/index.html', (err, html) => {
+//    if (err) throw err
+//
+//    http.createServer(function(request, response) {  
+//        response.writeHeader(200, {"Content-Type": "text/html"})
+//        response.write(html) 
+//        response.end()
+//    }).listen(htmlPort)
+//})
