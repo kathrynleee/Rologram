3b456cfdae3834cde93e95606ef3be598bd5aac4
Kathryn <kathrynleee@users.noreply.github.com>
2021-04-13 23:02:43 +0200

    create guide and swiper

--- public/script/tools.js
+++ public/script/tools.js
@@ -430,3 +430,18 @@ const updateDependencyType = (type) => {
 // const redo = () => {
   
 // }
+
+const showGuide = () => {
+  setVisible('.close', true, false)
+  setVisible('#guide', true, false)
+  const swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+}
