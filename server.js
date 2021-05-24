80a6e44f2a7a0e27f28e1fed013dbf5f497a8503
Kathryn <kathrynleee@users.noreply.github.com>
2021-05-24 19:47:09 +0200

    html to pug

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const pug = require('pug')
 let app = express()
 let port = process.env.PORT || 3000
 let dataController = require('./data')
@@ -8,16 +9,20 @@ let dataController = require('./data')
 app.options('*', cors())
 app.use(cors())
 app.use('/api/data', dataController)
+app.use(express.static('public'))
 // app.use('/api/data', databaseController)
-app.use(express.static(__dirname + '/public'))
+
+// app.use(express.static(__dirname + '/public'))
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html')
+// })
+
+app.set('views', './public/views')
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
+  res.render('_index')
 })
-
-// app.get('/api', (req, res) => {
-//   res.send('test')
-// })
 
 app.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`)
